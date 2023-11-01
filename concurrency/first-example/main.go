b4093f9211d60d80ad37232a5bbffa9501436275
Carlos <carlosmartcarr@gmail.com>
2023-11-01 17:39:42 +0000

    added waitgroup and test to our first example

--- concurrency/first-example/main.go
+++ concurrency/first-example/main.go
@@ -1,12 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	fmt.Println(s)
 }
 
 func main() {
-	printSomething("This is the first thing to be printed!")
-	printSomething("This is the second thing to be printed!")
+
+	// don't want to wait sleeping, we use WaitGroup instead
+	var wg sync.WaitGroup
+
+	// go routines has no order
+
+	words := []string{
+		"alpha",
+		"beta",
+		"gamma",
+		"delta",
+		"pi",
+		"eta",
+		"zeta",
+	}
+
+	wg.Add(len(words))
+
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d : %s", i, x), &wg)
+	}
+
+	wg.Wait()
+	wg.Add(1)
+	printSomething("This is the second thing to be printed!", &wg)
+
 }
